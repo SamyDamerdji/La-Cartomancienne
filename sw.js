caf8a2fc660acbe5ac1e9042599715f4001a1d01
SamyDamerdji <158787551+SamyDamerdji@users.noreply.github.com>
2025-06-11 13:50:20 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'cartomancie-v8'; // Nouvelle version pour forcer la mise à jour
-const urlsToCache = [
-  '/La-Cartomancienne/',
-  '/La-Cartomancienne/index.html',
-  '/La-Cartomancienne/manifest.json',
-  '/La-Cartomancienne/icons/icon-192.png',
-  '/La-Cartomancienne/icons/icon-512.png'
-];
-// ... le reste du code du service worker ...
